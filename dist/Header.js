54166c3cc74325006f3e0e2b67830b60d6298f0c
Qclid <skullmc101@gmail.com>
2025-12-17 14:54:46 +0530

    s

--- dist/Header.js
+++ dist/Header.js
@@ -1,18 +1,64 @@
-import React from 'react';
-import { Text, Box } from 'ink';
-import StarAnimation from './StarAnimation.js';
+import React from "react";
+import { Text, Box } from "ink";
+import DnaAnimation from "./DnaAnimation.js";
 const Header = () => {
-  return /*#__PURE__*/React.createElement(Box, {
-    flexDirection: "column",
-    alignItems: "center",
-    marginBottom: 1
-  }, /*#__PURE__*/React.createElement(Box, {
-    marginBottom: 1
-  }, /*#__PURE__*/React.createElement(StarAnimation, null)), /*#__PURE__*/React.createElement(Text, {
-    bold: true,
-    color: "cyan"
-  }, "AI HUB"), /*#__PURE__*/React.createElement(Text, {
-    color: "gray"
-  }, "Select your AI agent"));
+  return React.createElement(
+    Box,
+    {
+      flexDirection: "row",
+      padding: 1,
+      borderStyle: "single",
+      borderColor: "cyan"
+    },
+    React.createElement(DnaAnimation, null),
+    React.createElement(
+      Box,
+      { flexDirection: "column", justifyContent: "center" },
+      React.createElement(
+        Text,
+        { bold: true, color: "white" },
+        "PICK YOUR AGENT",
+      ),
+      React.createElement(Text, { color: "green", bold: true }, "FOR TODAY"),
+      React.createElement(
+
+
+        Box,
+
+
+        { marginTop: 1, flexDirection: "column", justifyContent: "center" },
+
+
+        React.createElement(
+
+
+          Text,
+
+
+          { color: "gray" },
+
+
+          "Use wasd/arrow keys to move",
+
+
+        ),
+
+
+        React.createElement(Text, { color: "gray" }, "And Enter to confirm"),
+
+
+      ),
+
+
+    ),
+
+
+  );
+
 };
+
+
+
+
+
 export default Header;
